c09e368ccb0eb02fcf1190fe424440b497c1ef86
Abigail91 <abigail_abarca@hotmail.com>
2021-09-02 14:09:48 -0600

    Add list of songs UI

--- UI/src/app/pages/songs/songs.component.ts
+++ UI/src/app/pages/songs/songs.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./songs.component.css']
 })
 export class SongsComponent implements OnInit {
-
+  songs = [ {
+    title: 'Love of my life',
+    artist: 'Queen',
+    id: 1
+  },
+  {
+    title: 'Aerials',
+    artist: 'System of a down',
+    id: 2
+  },
+  {
+    title: 'Scorpions',
+    artist: 'Wind of change',
+    id: 3
+  },
+  {
+    title: 'Bed of roses',
+    artist: 'Bon Jovi',
+    id: 4
+  }
+  ];
   constructor() { }
 
   ngOnInit(): void {
