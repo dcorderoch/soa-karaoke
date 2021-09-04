f962a8468b55e175b4095104b6f21106f3cdd546
Abigail91 <abigail_abarca@hotmail.com>
2021-09-03 18:42:16 -0600

    Add player UI

---
+++ UI/src/app/services/reproductor.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReproductorService {
+
+  constructor() { }
+
+  public secondsToString(seconds: number) {
+    let time = '';
+    const minute = Math.floor((seconds / 60) % 60);
+    if ( minute < 10 ){
+      time += '0' + minute.toString() + ':' ;
+    }else{
+      time += minute.toString() + ':' ;
+    }
+    const second = seconds % 60;
+    if ( second  < 10 ){
+      time += '0' + Math.trunc(second).toString();
+    }else{
+      time += Math.trunc(second).toString();
+    }
+    return time;
+}
+}
