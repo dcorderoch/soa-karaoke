f71fefedfa2591f80dcd3392302dedc96df50139
Abigail91 <abigail_abarca@hotmail.com>
2021-09-03 18:42:16 -0600

    Add player UI

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SongsComponent } from './pages/songs/songs.component';
+import { ReproductorComponent } from './pages/reproductor/reproductor.component';
+
 
 
 const routes: Routes = [{
@@ -11,6 +13,10 @@ const routes: Routes = [{
 {
   path: 'songs',
   component: SongsComponent
+},
+{
+  path: 'song/:id',
+  component: ReproductorComponent
 }];
 
 @NgModule({
