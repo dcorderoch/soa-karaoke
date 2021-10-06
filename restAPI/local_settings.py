054c4bbb6f8d3ed096cc970cb73673b8fb549f56
SMZ19 <sergio.197@hotmail.com>
2021-10-05 21:17:23 -0600

    Fix in gitignore and local_settings file added

---
+++ restAPI/local_settings.py
@@ -0,0 +1,11 @@
+# for local
+
+SECRET_KEY = b'SMZ19'
+
+SERVER_URL = "http://localhost:8080/auth/"
+ADMIN_USERNAME = "smz19"
+ADMIN_PASS = "smz19"
+REALM_NAME = "backEnd-Realm"
+CLIENT_ID = "backendClient"
+CLIENT_SECRET = "fe3b4bd4-2aa5-435c-b8e9-be3af38093d6"
+
